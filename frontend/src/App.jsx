db559ee47ff1e5e71818645d23a04820b94926ef
arnaudsm <arnaud.desaintmeloir@gmail.com>
2024-06-17 13:44:42 +0200

    Auto-Héberger les polices Google Fonts pour protéger la vie privée

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { useState, useMemo, useContext, createContext } from "react";
 import "./index.scss";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import {
   BottomNavigation,
