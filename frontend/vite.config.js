4e41f946dc1669fc6d93e96e77ea3c4618c0ed5c
Clément Gayot <contact@clement-gayot.fr>
2024-06-26 04:34:22 +0200

    :wrench: Add vite-plugin-mkcert plugin, to provide certificate support for vite https development services

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ include: "**/*.svg" })],
+  plugins: [react(), mkcert(), svgr({ include: "**/*.svg" })],
 });
