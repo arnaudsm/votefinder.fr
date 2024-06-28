ddb813f93e43b34266627deec20eb78e8a90abc7
Clément Gayot <contact@clement-gayot.fr>
2024-06-28 10:34:35 +0200

    :fire: Remove unused useGSAP from App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-
-gsap.registerPlugin(useGSAP);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
