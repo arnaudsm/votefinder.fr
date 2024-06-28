2ce86ce35d345896dbec59fca4201fdeb53d7d3c
Clément Gayot <contact@clement-gayot.fr>
2024-06-28 10:01:52 +0200

    :sparkles: ResultsListe : add total count

--- frontend/src/components/ResultsListes.jsx
+++ frontend/src/components/ResultsListes.jsx
@@ -1,14 +1,20 @@
 import ResultListe from "./ResultListe";
+import data from "../data/data.json";
 
 export default function ResultsListes({ results, choices }) {
+  const vote_length = Object.keys(data.votes).length;
+  const choices_length = Object.keys(choices).length;
+
   return (
     <div className="ResultsListes">
-      <div className="ResultsListes__explanation">
+      <p className="ResultsListes__explanation">
         Pourcentage d’accord avec les listes sortantes.
         <br />
-        Calculé sur {Object.keys(choices).length} votes. Continuez à voter pour
-        affiner vos résultats.
-      </div>
+        Calculé sur {choices_length} votes sur les {vote_length} disponibles.
+        {choices_length < vote_length && (
+          <> Continuez à voter pour affiner vos résultats !</>
+        )}
+      </p>
       {results.lists.map(([id, approval]) => (
         <ResultListe id={id} approval={approval} key={id} />
       ))}
